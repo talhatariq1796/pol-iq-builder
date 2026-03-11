d1e5060a25977e128ad349619f2aca06ef28807b
Talha tariq <talha@whiteboxtech.net>
2026-03-11 14:52:40 +0500

    refac: Remove cma from app

--- components/cma/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as CMAInterface } from './CMAInterface';
-export { default as CMAFilterDialog } from './CMAFilterDialog';
-export { default as CMAReport } from './CMAReport';
-export { default as SimpleMap } from './SimpleMap';
-export { default as AIAnalysisSection } from './AIAnalysisSection';
-export { useCMAData } from './hooks/useCMAData';
-export type * from './types';
